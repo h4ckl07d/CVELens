a858b0caa8dc809c26c5a6dfb0a8c6424d7fff09
h4ckl07d <lawalgbolahan042@gmail.com>
2026-06-03 01:50:54 +0100

    updated API key and Ai model

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 NVD_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
 CISA_KEV_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
-CLAUDE_MODEL = "claude-sonnet-4-20250514"
+GROQ_MODEL = "moonshotai/kimi-k2-instruct"
