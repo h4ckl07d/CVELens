66cf1359406f062e26ad119d9b8e6416d7b76ce1
h4ckl07d <lawalgbolahan042@gmail.com>
2026-06-03 12:29:50 +0100

    change ai model

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 NVD_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
 CISA_KEV_URL = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
-GROQ_MODEL = "moonshotai/kimi-k2-instruct"
+GROQ_MODEL = "openai/gpt-oss-120b"
